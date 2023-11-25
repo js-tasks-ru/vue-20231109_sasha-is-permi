552bf720849afc95b1a38f82be2eccc4877f186d
Alexandr <sasha-is-permi@mail.ru>
2023-11-25 22:35:46 +0500

    2 раздел- компоненты

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -24,6 +24,11 @@ export default defineComponent({
         day: 'numeric',
       });
     },
+  }, 
+  methods:{
+    formatAsIsoDate(timestamp) {
+      return new Date(timestamp).toISOString().substring(0, 10);
+    },
   },
 
   template: `
@@ -38,8 +43,9 @@ export default defineComponent({
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-    
+        <time :datetime="formatAsIsoDate(date)">
         {{ dateFormated }}
+        </time>
        
       </li>
     </ul>`,
