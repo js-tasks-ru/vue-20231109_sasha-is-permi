d3d0359bc98274f9d6f53523d1cd63904f2c1996
Alexandr <sasha-is-permi@mail.ru>
2023-12-10 20:24:16 +0500

    06-06-meetupView

--- 02-components/06-MeetupView/components/MeetupView.js
+++ 02-components/06-MeetupView/components/MeetupView.js
@@ -1,6 +1,11 @@
-import { defineComponent } from '../vendor/vue.esm-browser.js';
+import {defineComponent} from '../vendor/vue.esm-browser.js';
 import UiAlert from './UiAlert.js';
 import UiContainer from './UiContainer.js';
+import MeetupDescription from '../../02-MeetupDescription/components/MeetupDescription.js';
+import MeetupCover from '../../03-MeetupCover/components/MeetupCover.js';
+import MeetupInfo from '../../04-MeetupInfo/components/MeetupInfo.js';
+import MeetupAgendaItem from '../../05-MeetupAgenda/components/MeetupAgendaItem.js';
+import MeetupAgenda from '../../05-MeetupAgenda/components/MeetupAgenda.js';
 
 export default defineComponent({
   name: 'MeetupView',
@@ -8,6 +13,11 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupDescription,
+    MeetupCover,
+    MeetupInfo,
+    MeetupAgendaItem,
+    MeetupAgenda,
   },
 
   props: {
@@ -19,22 +29,28 @@ export default defineComponent({
 
   template: `
     <div>
-      <!-- meetup cover-->
+    <MeetupCover :title="meetup.title" :image="meetup.image"  />
 
       <UiContainer>
         <div class="meetup">
           <div class="meetup__content">
             <h3>Описание</h3>
-            <!-- meetup description -->
+          <MeetupDescription :description="meetup.description">
 
             <h3>Программа</h3>
-            <!-- meetup agenda -->
-            <UiAlert>Программа пока пуста...</UiAlert>
+            <MeetupAgenda v-if="meetup.agenda.length !== 0" :agenda="meetup.agenda" />
+            <UiAlert v-else-if="meetup.agenda.length === 0">Программа пока пуста...</UiAlert>
           </div>
           <div class="meetup__aside">
-            <!-- meetup info -->
+          <MeetupInfo 
+          :organizer="meetup.organizer"
+          :place="meetup.place"
+          :date="meetup.date"
+          />
           </div>
         </div>
       </UiContainer>
     </div>`,
 });
+
+
