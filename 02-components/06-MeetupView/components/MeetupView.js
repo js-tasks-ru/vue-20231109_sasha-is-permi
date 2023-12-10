a80c31502dd6b8fd30e7b3835efc7e3220d00e7d
Alexandr <sasha-is-permi@mail.ru>
2023-12-10 20:32:26 +0500

    06-06-meetupView

--- 02-components/06-MeetupView/components/MeetupView.js
+++ 02-components/06-MeetupView/components/MeetupView.js
@@ -38,7 +38,7 @@ export default defineComponent({
           <MeetupDescription :description="meetup.description">
 
             <h3>Программа</h3>
-            <MeetupAgenda v-if="meetup.agenda.length !== 0" :agenda="meetup.agenda" />
+            <MeetupAgenda :agenda="meetup.agenda" />
             <UiAlert v-else">Программа пока пуста...</UiAlert>
           </div>
           <div class="meetup__aside">
