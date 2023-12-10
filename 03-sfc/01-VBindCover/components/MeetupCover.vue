508c33829bc56db197d497930c393eb116307427
Alexandr <sasha-is-permi@mail.ru>
2023-12-11 02:30:24 +0500

    03-01 MeetupCover с v-bind

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+    <h1 class="meetup-cover__title"> {{ title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MeetupCover',
+  props: {
+    title: {
+      type:String,
+      required: false
+    },
+    image: {
+      type: String,
+      required: false
+    },
+  },
+  computed: {
+    backgroundImage() {
+        const imageUrl = this.image ? `url('${this.image}')` : 'var(--default-cover)'
+        return `linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), ${imageUrl}`
+    }
+  }
 };
 </script>
 
@@ -14,8 +30,7 @@ export default {
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
+  background-image: v-bind('backgroundImage');
   /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
   display: flex;
   flex-direction: column;
