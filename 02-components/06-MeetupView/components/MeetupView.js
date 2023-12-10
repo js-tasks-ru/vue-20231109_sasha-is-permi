6cd84777aa1cac247edebec5f036932fac70a257
Alexandr <sasha-is-permi@mail.ru>
2023-12-10 20:39:32 +0500

    06-06-meetupView

--- 02-components/06-MeetupView/components/MeetupView.js
+++ 02-components/06-MeetupView/components/MeetupView.js
@@ -1,11 +1,10 @@
 import {defineComponent} from '../vendor/vue.esm-browser.js';
 import UiAlert from './UiAlert.js';
 import UiContainer from './UiContainer.js';
-import MeetupDescription from '../../02-MeetupDescription/components/MeetupDescription.js';
-import MeetupCover from '../../03-MeetupCover/components/MeetupCover.js';
-import MeetupInfo from '../../04-MeetupInfo/components/MeetupInfo.js';
-import MeetupAgendaItem from '../../05-MeetupAgenda/components/MeetupAgendaItem.js';
-import MeetupAgenda from '../../05-MeetupAgenda/components/MeetupAgenda.js';
+import MeetupDescription from '../../02-MeetupDescription/components/MeetupDescription.js'
+import MeetupCover from '../../03-MeetupCover/components/MeetupCover.js'
+import MeetupInfo from '../../04-MeetupInfo/components/MeetupInfo.js'
+import MeetupAgenda from '../../05-MeetupAgenda/components/MeetupAgenda.js'
 
 export default defineComponent({
   name: 'MeetupView',
@@ -16,7 +15,6 @@ export default defineComponent({
     MeetupDescription,
     MeetupCover,
     MeetupInfo,
-    MeetupAgendaItem,
     MeetupAgenda,
   },
 
@@ -38,8 +36,8 @@ export default defineComponent({
           <MeetupDescription :description="meetup.description">
 
             <h3>Программа</h3>
-            <MeetupAgenda v-if='meetup.agenda.length' :agenda='meetup.agenda'/>
-            <UiAlert v-else>Программа пока пуста...</UiAlert>
+            <MeetupAgenda v-if="meetup.agenda.length" :agenda="meetup.agenda"/>
+            <UiAlert v-else>Программа пока пуста...</UiAlert>      
           </div>
           <div class="meetup__aside">
           <MeetupInfo 
